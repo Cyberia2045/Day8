019a41255e286377b3f248168741bf9ab762c4db
Cyberia 2045 <sagansociety86@gmail.com>
2017-04-13 15:14:46 -0400

    more DOM stuff

--- scripts.js
+++ scripts.js
@@ -1,9 +1,30 @@
-var buttons = document.getElementsByClassName("button");
+// var buttons = document.getElementsByClassName("button");
 
-	for (let i=0; i < buttons.length; i++) {
-		buttons[i].addEventListener("click", changeBtnText); //closes event listener
-	}; // closes the for loop
+// 	for (var i=0; i < buttons.length; i++) {
+// 		buttons[i].addEventListener("click", changeBtnText); //closes event listener
+// 	}; // closes the for loop
 
-	function changeBtnText(event) {
-		event.target.innerHTML = "I was clicked using event.";
-	}
+// 	function changeBtnText(event) {
+// 		// event.target.innerHTML = "I was clicked using event.";
+// 		console.log(event)
+// 	}
+
+// var boxes = document.getElementsByClassName("textBox");
+
+// 	for (var i=0; i < boxes.length; i++) {
+// 		boxes[i].addEventListener("click", boxing);
+// 	};
+
+// 	function boxing(event) {
+// 		event.target.innerHTML = "destroyed";
+// 	};
+
+var boxes = document.getElementsByClassName("textBox");
+
+	for (var i=0; i < boxes.length; i++) {
+		boxes[i].addEventListener("click", boxing);
+	};
+
+	function boxing(event) {
+		event.target.innerHTML = "destroyed";
+	};
